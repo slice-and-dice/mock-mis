960094380b5ef21e0ff2aa217d2a6b2f311ef3d0
Django Shelton <djangoshelton@gmail.com>
2017-12-20 15:13:26 +0000

    Moved logic to send controller

--- src/api/send/send.controller.js
+++ src/api/send/send.controller.js
@@ -1,4 +1,16 @@
-// Conversion
-// Potentially get list of LA stores from elsewhere
-// Push data to correct LA store
-// Return success message
+const { formatConverterService } = require('../../services/');
+const store = require('../../store');
+const winston = require('winston');
+
+module.exports = async (body) => {
+  winston.info('send controller called');
+  const convertedData = formatConverterService.convert(body.data, body.config.targetFormat);
+  try {
+    const result = await store.pushToLa(body.data, body.config.destinationLA);
+    console.log(result);    
+    return result;
+  } catch(err) {
+    winston.error(`send controller error: ${err}`);
+    return err.message;
+  }
+}
--- src/api/send/send.router.js
+++ src/api/send/send.router.js
@@ -4,8 +4,8 @@ const sendController = require('./send.controller.js');
 module.exports = () => {
   const router = Router();
 
-  router.post('', (req, res, next) => {
-    res.send(sendController(req.body));
+  router.post('', async (req, res, next) => {
+    res.send(await sendController(req.body));
   });
 
   return router;
