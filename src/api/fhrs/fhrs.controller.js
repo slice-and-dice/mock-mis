56d5719665ec18b78ce60425284ec23c1a1d978e
Django Shelton <djangoshelton@gmail.com>
2018-01-16 16:33:15 +0000

    add getEstablishmentsFromLa function

--- src/api/fhrs/fhrs.controller.js
+++ src/api/fhrs/fhrs.controller.js
@@ -4,7 +4,7 @@ const { loggingService } = require('../../services');
 const getAllEstablishments = async () => {
   loggingService.logFunctionCall('fhrs.controller', 'getAllEstablishments');
   try {
-    const result = await connector.getAllEstablishments();
+    const result = await connector.getAllBasic('Establishments');
     loggingService.logFunctionSuccess('fhrs.controller', 'getAllEstablishments');
     return result;
   } catch (err) {
@@ -13,6 +13,25 @@ const getAllEstablishments = async () => {
   }
 }
 
+const getEstablishmentsFromLa = async (la) => {
+  loggingService.logFunctionCall('fhrs.controller', 'getEstablishmentsFromLa');
+  try {
+    const allAuthorities = await connector.getAllBasic('Authorities');
+    const laSummary = allAuthorities.authorities.filter(authority => authority.Name === la);
+    const searchParams = {
+      name: '',
+      localAuthorityId: laSummary[0].LocalAuthorityId,
+    }
+    const establishments = await connector.establishmentSearch(searchParams);
+    loggingService.logFunctionSuccess('fhrs.controller', 'getEstablishmentsFromLa');
+    return establishments;
+  } catch (err) {
+    loggingService.logFunctionError('fhrs.contoller', 'getEstablishmentsFromLa');
+    return err;
+  }
+}
+
 module.exports = {
   getAllEstablishments,
+  getEstablishmentsFromLa,
 }
