c74105ffcca7a897c20a1371213a79b5c90757ef
Django Shelton <djangoshelton@gmail.com>
2018-01-16 14:07:38 +0000

    add fhrs route to app

--- src/api/routers.js
+++ src/api/routers.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const sendRouter = require('./send');
 const retrieveRouter = require('./retrieve');
+const fhrsRouter = require('./fhrs');
 
 module.exports = () => {
   const router = Router();
 
   router.use('/send', sendRouter());
   router.use('/retrieve', retrieveRouter());
+  router.use('/fhrs', fhrsRouter());
 
   return router;
 };
