d79aa465ff929f34b7f5c2000e60f2fb5cee6422
Django Shelton <djangoshelton@gmail.com>
2018-01-16 16:33:32 +0000

    add route to get establishments from specific las

--- src/api/fhrs/fhrs.router.js
+++ src/api/fhrs/fhrs.router.js
@@ -1,4 +1,4 @@
-const Router = require('express');
+const { Router } = require('express');
 const fhrsController = require('./fhrs.controller');
 
 module.exports = () => {
@@ -6,6 +6,10 @@ module.exports = () => {
 
   router.get('', async (req, res) => {
     res.send(await fhrsController.getAllEstablishments());
+  });
+
+  router.get('/:la', async (req, res) => {
+    res.send(await fhrsController.getEstablishmentsFromLa(req.params.la));
   });
 
   return router;
