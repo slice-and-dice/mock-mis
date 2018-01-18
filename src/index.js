5be18a3ee02296486a8481d748dce1495420ccc7
Kiran Smith <kiran.smith@dwp.gsi.gov.uk>
2018-01-18 10:42:09 +0000

    Added base config for mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 const { info } = require('winston');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGO_URL } = process.env;
+
+const setupDB = async url => {
+  if (url) {
+    await mongoose.connect(MONGO_URL);
+  }
+}
+
+setupDB(MONGO_URL);
 
 app().listen(PORT, () => info(`App listening on port ${PORT}`));
