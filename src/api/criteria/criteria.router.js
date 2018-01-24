8e998b61bce710aa64244209198b430b4209e07a
Django Shelton <djangoshelton@gmail.com>
2018-01-24 10:18:43 +0000

    add get and update to criteria router

--- src/api/criteria/criteria.router.js
+++ src/api/criteria/criteria.router.js
@@ -8,5 +8,13 @@ module.exports = () => {
     res.send(await criteriaController.addCriteria(req.body));
   });
 
+  router.get('', async (req, res) => {
+    res.send(await criteriaController.getCriteria());
+  });
+
+  router.post('/update', async (req, res) => {
+    res.send(await criteriaController.updateCriteria(req.body));
+  });
+
   return router;
 }
