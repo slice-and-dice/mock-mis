ed402acb0c10611e996d387809361dba1b32682d
Django Shelton <djangoshelton@gmail.com>
2018-01-24 10:20:58 +0000

    update and calculate risk

--- src/services/potential-risk-engine.service.js
+++ src/services/potential-risk-engine.service.js
@@ -2,35 +2,72 @@ const Criteria = require('../models/Criteria');
 const { logFunctionSuccess } = require('./logging.service');
 const mongoose = require('mongoose');
 
+const options = {
+  useMongoClient: true
+};
+
+mongoose.connect(process.env.RISK_RULES_DB_URL, options);
+
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 
-const getCriteria = async body => {
+const getCriteria = async () => 
+  new Promise((resolve) => {
 
-  return new Promise((resolve, reject) => {
+    const criteriaData = Criteria.find({});
 
     logFunctionSuccess(
       'potential-risk-engine.service',
       'getCriteria'
     );
-
-    const criteriaData = await Criteria.find({});
-
-    resolve(body);
+    resolve(criteriaData);
   });
-}
 
-const addCriteria = async body => {
-  return new Promise((resolve, reject) => {
+const addCriteria = async body => 
+  new Promise((resolve) => {
+    const criteria = new Criteria();
+    criteria.riskType = body.riskType;
+    criteria.riskValue = body.riskValue;
+    const savedCriteria = criteria.save();
     logFunctionSuccess(
       'potential-risk-engine.service',
       'addCriteria'
     );
+    resolve(savedCriteria);
   });
 
-  const savedCriteria = await Criteria.save(body);
+const updateCriteria = async body => 
+  new Promise((resolve) => {
+    console.log(body);
+    const updatedCriteria = Criteria.findOneAndUpdate(
+      { riskType: body.riskType }, 
+      { riskType: body.riskType, riskValue: body.riskValue }
+    );
+    logFunctionSuccess(
+      'potential-risk-engine.service',
+      'updateCriteria'
+    );
+    resolve(updatedCriteria);
+  });
 
-  resolve(savedCriteria);
-}
+const calculateRisk = async (data) => {
+  const riskRules = await getCriteria();
+  return new Promise((resolve) => {
+    const riskScores = [];
+    riskRules.forEach((rule) => {
+      if (data[rule.riskType] === true) {
+        riskScores.push(rule.riskValue);
+      }
+    });
+    const riskObject = {riskScore: riskScores.reduce(((a, b) => a + b), 0) };
+    const enrichedData = Object.assign(riskObject, data);
+    logFunctionSuccess(
+      'potential-risk-engine.service',
+      'calculateRisk'
+    );
+    resolve(enrichedData);
+  });
+  
+};
 
-module.exports = { addCriteria, getCriteria }
+module.exports = { addCriteria, getCriteria, updateCriteria, calculateRisk };
